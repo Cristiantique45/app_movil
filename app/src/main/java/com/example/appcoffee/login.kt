0ef76614d2236d5992a245a582fd690aa613c4bb
cristiantique45 <cris2001tique@gmail.com>
2022-12-02 12:17:27 -0500

    clase app noviembre 02 avance

--- app/src/main/java/com/example/appcoffee/login.kt
+++ app/src/main/java/com/example/appcoffee/login.kt
@@ -1,11 +1,49 @@
 package com.example.appcoffee
+import android.content.Intent
+import android.net.wifi.p2p.WifiP2pManager.ActionListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuth.AuthStateListener
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class login : AppCompatActivity() {
+    private lateinit var firebaseAuth : FirebaseAuth
+    private lateinit var authStateListener : FirebaseAuth.AuthStateListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val btnsesion : Button = findViewById(R.id.btnsesion)
+        val txtUsuario : EditText = findViewById(R.id.txtusuario)
+        val txtContrasena : EditText = findViewById(R.id.txtcontrasena)
+        firebaseAuth = Firebase.auth
+        btnsesion.setOnClickListener()
+        {
+            singIn(txtUsuario.text.toString(), txtContrasena.text.toString())
+
+        }
+    }
+    private fun singIn (email:String, password: String)
+    {
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){ task ->
+                if(task.isSuccessful){
+                    val user = firebaseAuth.currentUser
+                    Toast.makeText(baseContext, "Autenticacion Exitosa", Toast.LENGTH_SHORT).show()
+                    //aqui se va a la pantalla principal
+                    val i = Intent(this,pantallaPrincipal::class.java)
+                    startActivity(i)
+                }
+                else{
+                    Toast.makeText(baseContext, "Error de correo y/o contraseña", Toast.LENGTH_SHORT).show()
+                }
+
+        }
     }
 }
